1ac7fad3f98b6dc7a0d9fad4a7e876278d539e28
Saulm <Saulm@SAUL>
2023-08-31 21:46:54 -0600

    Agregar archivos de proyecto.

---
+++ OrdenarMemoria/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OrdenarMemoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(12, 12);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(156, 65);
+            this.btnbuscar.TabIndex = 0;
+            this.btnbuscar.Text = "Seleccionar Memoria";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(59, 96);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(641, 316);
+            this.lista.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnbuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.ListBox lista;
+    }
+}
+
---
+++ OrdenarMemoria/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenarMemoria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    string[] files = Directory.GetFiles(selectedPath);
+
+                    lista.Items.Clear();
+                    foreach (string filePath in files)
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        lista.Items.Add(fileName);
+                    }
+                }
+            }
+        }
+    }
+}
